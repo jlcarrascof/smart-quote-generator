990e915618f621a003489695d7bc2dcebb446454
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-03-10 20:35:17 -0400

    Add initial plugin structure for Smart Quote Generator

---
+++ smart-quote-generator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Smart Quote Generator 
+ * Plugin URI: https://github.com/jlcarrascof/smart-quote-generator.
+ * Description: Generates embeddable quotes with shortcodes and REST API.
+ * Version: 1.0.0
+ * Author: Javier J. Martínez F.
+ * License: GPL v2 or later
+ * Text Domain: smart-quote
+ *  
+ */
+
+if ( ! defined( 'ABSPATH')) exit;
+
+?>
